f735d1bfda859e4b591d6adb201a08a1d9df6f00
nevespgabriel <nevespgabriel@gmail.com>
2024-06-13 10:30:43 -0300

    feat: verificaNum

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ let n;
 console.log("Digite o número que deseja exibir a tabuada:")
 process.stdin.on("data", function(data){
     n = data.toString().trim();
-    for(let c=1; c<11; c++){
-        console.log(n + " x " + c + " = " + n*c);
+    if(isNaN(n)){
+        console.log("O valor digitado não é um número. Digite novamente:");
+    } else{
+        for(let c=1; c<11; c++){
+            console.log(n + " x " + c + " = " + n*c);
+        }
+        process.exit();
     }
-    process.exit();
 });
